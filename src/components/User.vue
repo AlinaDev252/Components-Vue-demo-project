70d8787ef6dacbe337ce53309615477c3129c214
AlinaDev252 <costachealina25@gmail.com>
2021-06-18 14:47:02 +0300

    Add computed properties for User component

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,15 +1,22 @@
 <template>
 	<button type="button" @click="onClickAge">Update age</button>
 	<p>The user is {{ age }} years old</p>
+	<p>{{ ageDoubled }}</p>
 </template>
 
 <script>
 export default {
 	name: "User",
 	props: ["age"],
+	emits: ["age-change"],
+	computed: {
+		ageDoubled() {
+			return this.age * 2;
+		},
+	},
 	methods: {
 		onClickAge() {
-			this.$emit("age-change");
+			this.$emit("age-change", 3);
 		},
 	},
 };
